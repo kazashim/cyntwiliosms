445f83c27cf71bda9e1cd86b431649052f0e52a3
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-24 13:05:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,44 +11,25 @@
 <body>
 
 <h2>Cyn Twilio sms Example</h2>
-<p>Resize the browser window to see the effect. When the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other.</p>
+<p>you will get to know how to send sms using twilio.</p>
 
 <div class="container">
-  <form action="/action_page.php">
+  <form action="method "post">
   <div class="row">
     <div class="col-25">
-      <label for="fname">First Name</label>
+      <label for="fname">Mobile number</label>
     </div>
     <div class="col-75">
-      <input type="text" id="fname" name="firstname" placeholder="Your name..">
+      <input type="text" id="fname" name="mobile number" placeholder="Mobile number">
     </div>
   </div>
+  
   <div class="row">
     <div class="col-25">
-      <label for="lname">Last Name</label>
+      <label for="subject">Message</label>
     </div>
     <div class="col-75">
-      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-25">
-      <label for="country">Country</label>
-    </div>
-    <div class="col-75">
-      <select id="country" name="country">
-        <option value="australia">Australia</option>
-        <option value="canada">Canada</option>
-        <option value="usa">USA</option>
-      </select>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-25">
-      <label for="subject">Subject</label>
-    </div>
-    <div class="col-75">
-      <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+      <textarea id="subject" name="msgs" placeholder="your message" style="height:200px"></textarea>
     </div>
   </div>
   <div class="row">
