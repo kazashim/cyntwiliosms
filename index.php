8dd2e54f43e5544db6337c648fdbf642fcfe322a
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-26 15:27:02 +0200

    update

--- index.php
+++ index.php
@@ -1,61 +1,51 @@
 <!-- ===== Our PHP code will be here ===== -->
 <?php 
     include './vendor/autoload.php';
-if (isset($_POST['mobile']) && isset($_POST['msgs'])){
+	use Twilio\Rest\Client;
+	
+if(isset($_POST['mobile']) && isset($_POST['msg'])){
 
 // Twilio
 
-$sid ='AC6ea1c5874e50c0ce1b4baf7a16063803';
-$token='75129491c1c975859870d93e3eb967f6';
+$account_sid = 'MGb77f89a0b2998d0bac62e1da2df425dc';
+$auth_token = '75129491c1c975859870d93e3eb967f6';
 
-$client = New Twilio/Rest/Client($sid, $token);
-$message = $client ->message->create(
+$client = new Client($account_sid, $auth_token);
+$messages = $client->messages->create(
     $_POST['mobile'],array(
         'from' => '+19088670450',
-        'body' => $_POST['msgs']
+        'body' => $_POST['msg']
          )
     );
+if ($messages->sid){
+	
+echo "message sent";
 
-    if($message->sid){
-            echo "message sent successfully";
-
-    }
+};
+    
 }
 
 ?>
-<link href="style.css" rel="stylesheet">
-<html>
-<head>
-</head>
-<body>
 
+
+<html>
 <h2>Cyn Twilio sms Example</h2>
 <p>you will get to know how to send sms using twilio.</p>
 
-<div class="container">
-  <form action="" method "post">
-  <div class="row">
-    <div class="col-25">
-      <label for="fname">Mobile number</label>
-    </div>
-    <div class="col-75">
-      <input type="text" id="fname" name="mobile" placeholder="Mobile number">
-    </div>
-  </div>
-  
-  <div class="row">
-    <div class="col-25">
-      <label for="subject">Message</label>
-    </div>
-    <div class="col-75">
-      <textarea id="subject" name="msgs" placeholder="your message" style="height:200px"></textarea>
-    </div>
-  </div>
-  <div class="row">
-    <input type="submit" value="Submit">
-  </div>
-  </form>
-</div>
 
-</body>
+  <form action="" method="post">
+
+     Mobile number
+    <br>
+   
+      <input type="text" placeholder="Mobile number" name="mobile">
+    <br>
+	
+  Message<br>
+   
+      <textarea id="subject" placeholder="your message" name="msg"></textarea>
+   
+    <input type="submit" value="Submit">
+  
+  </form>
 </html>
